5faf5d5ee26ffea343c0d654992157219eacc8c3
zk <zachary.kim@gmail.com>
2013-03-27 14:01:16 -0700

    Add deploy script, dequality project name

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.clojars.soundcloud/clj-exceptional "0.8.0"
+(defproject clj-exceptional "0.8.0"
   :description "Clojure client for Exceptional http://getexceptional.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "5.0.1"]
                  [clj-http "0.6.4"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
-                     [lein-clojars "0.6.0"]])
+                     [lein-clojars "0.6.0"]]
+  :jar-name "clj-exceptional.jar")
